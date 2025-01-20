0cd22b14bbf47ca5a7609379bf420dc6896c1ead
yaerdandan <1685086963@qq.com>
2025-01-20 17:07:18 +0800

    feat: 测试

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import App from "./App.vue";
+console.log(1);
 
 const app = createApp(App);
 app.use(ElementPlus);
